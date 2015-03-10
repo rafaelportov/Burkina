2929d23db4d5acd70fd1cd1faa340aef4291c861
rafaelportov <rafaelportov@gmail.com>
2015-03-10 12:45:19 -0300

    Commit Blank Solution

---
+++ Burkina.Donations.Test/UnitTestBurkina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Burkina.Donations.Test
+{
+    [TestClass]
+    public class UnitTestBurkina
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Burkina.Donations.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Burkina.Donations.Web.MvcApplication" Language="C#" %>
