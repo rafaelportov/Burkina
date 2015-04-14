71609d283fc975c0697de529f5a920bad9cdf5c4
rafaelportov <rafaelportov@gmail.com>
2015-04-14 14:37:44 -0300

    Backup1404

--- Burkina.Donations.Web/App_Start/RouteConfig.cs
+++ Burkina.Donations.Web/App_Start/RouteConfig.cs
@@ -16,8 +16,10 @@ namespace Burkina.Donations.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Home", action = "DonationsTotal", id = UrlParameter.Optional }
-                // defaults: new { controller = "Home", action = "Doadores", id = UrlParameter.Optional }
+                defaults: new {controller = "Donation", action = "Index", id = UrlParameter.Optional }
+                //Nesse caso, a página default está: http://localhost:51945/Donation/Index 
+                //Se eu não informar nenhum controller por exemplo, ele acessa por default o controller Home.
+                //Se eu não informar nenhuma action por exemplo, ele acessa por default a action DonationsTotal.
             );
         }
     }
